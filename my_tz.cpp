7471809528a12e3d51f4e68ae531f0968acbf22c
tatsche <tatsche@gmx.de>
2022-04-12 15:21:39 +0200

    Update my_tz.cpp to work with AceTime v1.9.0

--- my_tz.cpp
+++ my_tz.cpp
@@ -8,24 +8,30 @@ using namespace ace_time;
 
 // Create a BasicZoneManager with the entire TZ Database.
 static const int CACHE_SIZE = 3;
-static BasicZoneManager<CACHE_SIZE> manager(
-    zonedb::kZoneRegistrySize, zonedb::kZoneRegistry);
+BasicZoneProcessorCache<CACHE_SIZE> zoneProcessorCache;
+static BasicZoneManager zoneManager(
+    zonedb::kZoneRegistrySize,
+    zonedb::kZoneRegistry,
+    zoneProcessorCache);
+
+//static BasicZoneManager<CACHE_SIZE> manager(
+//    zonedb::kZoneRegistrySize, zonedb::kZoneRegistry);    
 
 void init_timezones(void) {
-  auto GMT_tz = manager.createForZoneId(TZ_GMT);
-  auto homeTz = manager.createForZoneId(general_config.home_tzindex);
-  auto localTz = manager.createForZoneId(general_config.local_tzindex);
+  auto GMT_tz = zoneManager.createForZoneId(TZ_GMT);
+  auto homeTz = zoneManager.createForZoneId(general_config.home_tzindex);
+  auto localTz = zoneManager.createForZoneId(general_config.local_tzindex);
 }
 
 // get time, convert to a "local" tz
 void get_time_in_tz(uint32_t tzoneId) {
-  auto GMT_tz = manager.createForZoneId(TZ_GMT);
+  auto GMT_tz = zoneManager.createForZoneId(TZ_GMT);
   tnow = ttgo->rtc->getDateTime();
   auto GMT_Time = ZonedDateTime::forComponents(
     tnow.year, tnow.month, tnow.day,
     tnow.hour, tnow.minute, tnow.second, GMT_tz);
   // systemClock.setNow(GMT_Time.toEpochSeconds());
-  auto localTz = manager.createForZoneId(tzoneId);
+  auto localTz = zoneManager.createForZoneId(tzoneId);
   auto localTime = GMT_Time.convertToTimeZone(localTz);
   hh     = localTime.hour();
   mm     = localTime.minute();
@@ -40,7 +46,7 @@ void convert_alarm_time_to_gmt(int8_t *ahhp, int8_t *ammp) {
   tnow = ttgo->rtc->getDateTime();
   tnow.hour = general_config.alarm_h;
   tnow.minute = general_config.alarm_m;
-  auto local_tz = manager.createForZoneId(general_config.local_tzindex);
+  auto local_tz = zoneManager.createForZoneId(general_config.local_tzindex);
   auto Alarm_Time = ZonedDateTime::forComponents(
     tnow.year, tnow.month, tnow.day,
     tnow.hour, tnow.minute, tnow.second, local_tz);
@@ -48,7 +54,7 @@ void convert_alarm_time_to_gmt(int8_t *ahhp, int8_t *ammp) {
   acetime_t epochSeconds = Alarm_Time.toEpochSeconds();
   // Serial.print("Epoch seconds: ");
   // Serial.println(epochSeconds);
-  auto alarmTz = manager.createForZoneId(TZ_GMT);
+  auto alarmTz = zoneManager.createForZoneId(TZ_GMT);
   auto alarmTime = ZonedDateTime::forEpochSeconds(epochSeconds, alarmTz);
   // Serial.print(F("Alarm Time (GMT): "));
   // alarmTime.printTo(Serial);
